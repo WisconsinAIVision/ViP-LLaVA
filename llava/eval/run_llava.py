ab4e364125ba55767d35025ca3af6ee5b90b69e6
Mu Cai <65588556+mu-cai@users.noreply.github.com>
2024-06-12 17:14:02 -0600

    Update run_llava.py

--- llava/eval/run_llava.py
+++ llava/eval/run_llava.py
@@ -125,14 +125,15 @@ def eval_model(args):
         )
 
     input_token_len = input_ids.shape[1]
-    n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
-    if n_diff_input_output > 0:
-        print(
-            f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids"
-        )
-    outputs = tokenizer.batch_decode(
-        output_ids[:, input_token_len:], skip_special_tokens=True
-    )[0]
+    # n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+    # if n_diff_input_output > 0:
+    #     print(
+    #         f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids"
+    #     )
+    # outputs = tokenizer.batch_decode(
+    #     output_ids[:, input_token_len:], skip_special_tokens=True
+    # )[0]
+    outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
     outputs = outputs.strip()
     if outputs.endswith(stop_str):
         outputs = outputs[: -len(stop_str)]
